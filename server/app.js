4f4e511f39905e272dac8562d10c35950d71142a
harshikadhanda <harshikadhanda05@gmail.com>
2026-09-12 20:13:20 +0530

    update

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const reportRouter = require('./routes/reportRouter');
 
 
 app.use(cors({
-    origin: ['http://localhost:3000','https://placement-management-backend-ha3i.onrender.com','http://localhost:5173','https://placement-frontend-yre7.onrender.com'],
+    origin: ['http://localhost:3000','http://localhost:5173','https://placement-frontend-yre7.onrender.com'],
     methods:['GET','POST', 'DELETE', 'UPDATE', 'PUT'],
     credentials:true
 }))
